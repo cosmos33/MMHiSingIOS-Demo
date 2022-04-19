60e4135fa2ffe88bfd9a589f7f26ec39c2f31f9f
yanbo_momo <yanbo@immomo.com>
2022-04-19 20:52:34 +0800

    更新 SDK

--- MMHiSingDemo/MMHiSingDemo/ViewController.m
+++ MMHiSingDemo/MMHiSingDemo/ViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "ViewController.h"
+#import <MMHiSingKit/MMHiSingClient.h>
 
 @interface ViewController ()
+
+@property (nonatomic, strong) MMHiSingClient *client;
 
 @end
 
@@ -15,6 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.client = [[MMHiSingClient alloc] initSDKWithContentView:self.view roomId:@"xxx" controller:self channelId:@"xxx" andSeatCount:8 appId:@"xxx"];
     // Do any additional setup after loading the view.
 }
 
---
+++ MMHiSingDemo/Pods/Target Support Files/Pods-MMHiSingDemo/Pods-MMHiSingDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MMHiSingDemo : NSObject
+@end
+@implementation PodsDummy_Pods_MMHiSingDemo
+@end
